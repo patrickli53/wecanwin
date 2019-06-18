50daa7cee1ace6f3a0ac8fb0504eb2a737c27d94
Patrick Li <lip53@mcmaster.ca>
2019-06-18 10:28:20 -0400

    first commit on new branch

--- src/landingcomponents/Whatpage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Whatpage = () => {
-    return(
-        <div className='whatpage' id='what'>what we do</div>
-    )
-}
-
-export default Whatpage;
--- src/landingcomponents/Wholanding.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Wholanding = () => {
-    return(
-        <div className='wholand' id='who'>who we are</div>
-    )
-}
-
-export default Wholanding;
