ccbbe493baeecc93298a0ec10d68f07b18b7043e
Patrick Li <lip53@mcmaster.ca>
2019-07-02 09:51:50 -0400

    added icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return(
-        <div className='footer'>footer</div>
+        <div className='footer'>
+            <div>Icons made by <a href="https://www.flaticon.com/authors/catkuro" title="catkuro">catkuro</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+            
+        </div>
+        
     )
 }
 
--- src/components/Whatpage.js
+++ src/components/Whatpage.js
@@ -9,6 +9,11 @@ const Whatpage = () => {
                 <h1 className=' f1 ttu pl6 pt5'>
                     <u>What We Do</u>
                 </h1>
+                <div className="pl6 pt5 ">
+                    <img
+                        src={require('./create.svg')}
+                        className="br-100 ba b--blue h3 w3 dib" alt="create" />
+                </div>
 
                 <h3 className='fw4 baskerville pl6 pt3'> <u>This Week's Flyer</u></h3>
                 <img className ='pt2 pb2 ml6' src={require('./flyer.png')} height={250} width={200}/>
