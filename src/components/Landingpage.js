4e6faccd18f9326dc3e9449c798782076972d7b5
Patrick Li <lip53@mcmaster.ca>
2020-02-21 16:32:57 -0500

    Fixed npm issues

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -6,7 +6,6 @@ class Landingpage extends Component{
         return(
             <div className='landingpage tc bg'>
                 <h1 className='title'> <span>Empowering Youth, Empowering Seniors</span></h1>
-                <h3 className='subtitle'><span>Empowering the Community</span> </h3>
             </div>
 
         )
